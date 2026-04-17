ceca03192a618e345453ca0a571001af62bd6b9c
Charchit subedi <59109697+charchit-subedi@users.noreply.github.com>
2026-04-18 04:22:26 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCTYa000tqEDgGt9W0LCxm2_A2fWS1Ph7Y",
@@ -8,10 +9,12 @@ const firebaseConfig = {
   storageBucket: "khanji-e7b25.firebasestorage.app",
   messagingSenderId: "18693432968",
   appId: "1:18693432968:web:1e06923669eaf68777213d",
-  measurementId: "G-H87CWX8Y9L"
+  measurementId: "G-H87CWX8Y9L"// Your existing config
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db };
+export { db, auth };
+
